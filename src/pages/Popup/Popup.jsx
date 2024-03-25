113a854c5fc6566a3f34e74ca82f07d6208ba44a
Ancss <61501274+Ancss@users.noreply.github.com>
2024-03-25 15:45:28 +0800

    update icons file

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../assets/img/logo.svg';
+import logo from '../../assets/img/logo.png';
 import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
 
